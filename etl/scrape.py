0d6f4a0ae6b58b66bb3e7ea70e4939381cd9b6a8
nahumsa <nahumsaa@gmail.com>
2021-06-05 11:21:39 -0300

    refactored scrape_tweets
    Now it is a class `scrapeTweets` that has a dataframe and a `save_csv` method

--- etl/scrape.py
+++ etl/scrape.py
@@ -2,18 +2,25 @@ import twint
 import pandas as pd
 
 
-def scrape_tweets(username: str, n_tweets: int) -> pd.DataFrame:
-    
-    c = twint.Config()
-    c.Limit = n_tweets
-    c.Username = username
-    c.Pandas = True
-    c.Hide_output = True
+class scrapeTweets():
 
-    twint.run.Search(c)
+    def __init__(self, username: str, n_tweets: int) -> None:
+        self._tweet_df = self._scrape(username, n_tweets)
 
-    return twint.storage.panda.Tweets_df
-    
+    @property
+    def df(self):
+        return self._tweet_df
 
-if __name__ == "__main__":
-    scrape_tweets("sa_nahum", 10).to_csv("nahum.csv")
+    def _scrape(self, username: str, n_tweets: int) -> pd.DataFrame:
+        c = twint.Config()
+        c.Limit = n_tweets
+        c.Username = username
+        c.Pandas = True
+        c.Hide_output = True
+
+        twint.run.Search(c)
+
+        return twint.storage.panda.Tweets_df
+
+    def save_csv(self, path):
+        self._tweet_df.to_csv(path)
--- test/test_scrape.py
+++ test/test_scrape.py
@@ -1,9 +1,20 @@
 import unittest
-from etl.scrape import scrape_tweets
+import os
+from etl.scrape import scrapeTweets
 
 
 class TestScrape(unittest.TestCase):
 
-    def test_check_df(self):
-        df_scrape = scrape_tweets("sa_nahum", 10)
+    def setUp(self):
+        self.scrape_obj = scrapeTweets("sa_nahum", 10)
+        self.save_path = "n.csv"
+    
+    def test_check_df_columns(self):
+        df_scrape = self.scrape_obj.df
         self.assertIn("tweet", df_scrape.columns)
+        self.assertIn("date", df_scrape.columns)
+        self.assertIn("timezone", df_scrape.columns)
+    
+    def test_data_save(self):
+        self.scrape_obj.save_csv(self.save_path)
+        self.assertIn(self.save_path, os.listdir())
