452681aaf28eda1a69d7e82b90fb273ed057342b
nahumsa <nahumsaa@gmail.com>
2021-06-11 18:22:00 -0300

    add test for clean_df

--- etl/process_df.py
+++ etl/process_df.py
@@ -15,7 +15,6 @@ def clean_df(path: str, keep_columns:list) -> pd.DataFrame:
     """
 
     df = pd.read_csv(path)
-    
     tweet_df = df[keep_columns]
 
     if "date" in tweet_df.columns:
@@ -25,14 +24,24 @@ def clean_df(path: str, keep_columns:list) -> pd.DataFrame:
         raise ValueError("The dataframe doesn't have a date.")
 
 
+ 
+def convert_timezone(df:pd.DataFrame) -> pd.DataFrame:
+    """Convert timezones given a dataframe with `date`
+    and `timezone` columns.
+
+    Args:
+        df (pd.DataFrame): [description]
+
+    Returns:
+        pd.DataFrame: [description]
+    """
+    return df["date"] + datetime.timedelta(hours=df['timezone']/100)
+
 def convert_time(df: pd.DataFrame) -> pd.DataFrame:
     # convert datetime
     df['date'] = pd.to_datetime(df["date"])
-
+    
     # apply timezone to convert to UTC
-    def convert_timezone(df):
-        return df["date"] - datetime.timedelta(hours=df['timezone']/100)
-
     df["date"] = df.apply(convert_timezone, axis=1)
 
     df = df.drop("timezone", axis=1)
@@ -40,5 +49,6 @@ def convert_time(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 if __name__ == "__main__":
-    keep_columns = ["date", "timezone", "tweet", "language"]
+    keep_columns = ["id", "date", "timezone", "tweet", "language"]
+    print(pd.read_csv("nahum.csv"))
     print(clean_df("nahum.csv", keep_columns))
---
+++ test/test_process_df.py
@@ -0,0 +1,35 @@
+import unittest
+import os
+import pandas as pd
+import datetime
+
+from etl.process_df import clean_df, convert_time
+
+
+class TestCleanDF(unittest.TestCase):
+
+    def test_convert_timezone(self):
+        rng = pd.date_range('2015-02-24', periods=5, freq='T')
+        df = pd.DataFrame({ 'date': rng, 'timezone': +100 }) 
+        
+        exp_df = df.copy()
+        exp_df['date'] += datetime.timedelta(hours=+1)
+        exp_df = exp_df.drop("timezone", axis=1)
+
+        got_df = convert_time(df)
+
+        pd.testing.assert_frame_equal(got_df, exp_df)
+
+    def test_check_no_date(self):
+        with self.assertRaises(ValueError):
+            d = {'col1': [1, 2], 'col2': [3, 4]}
+            df = pd.DataFrame(data=d)
+            
+            df.to_csv("mock.csv", index=False)
+            
+            clean_df("mock.csv", ['col1'])
+            
+    def tearDown(self):
+        if "mock.csv" in os.listdir():
+            os.remove("mock.csv")
+    
