ff53910ac74db7779100fa9ca49b897da3df74e5
nahumsa <nahumsaa@gmail.com>
2021-07-03 17:24:35 -0300

    add full workflow to airflow

--- dags/etl_dag.py
+++ dags/etl_dag.py
@@ -1,11 +1,16 @@
+import psycopg2
+import pandas as pd
+
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 
 from etl.scrape import scrapeTweets
 from etl.process_df import clean_df
+from etl.database import create_table, add_df_to_sql
+from sqlalchemy import create_engine
 
-from functools import partial
+TABLE_NAME = "tweet_body"
 
 def extract_tweets():
     scrape_time = datetime.now().isoformat()
@@ -15,15 +20,52 @@ def extract_tweets():
     return name_file
 
 def clean_data(ti):
+    # Get  the first element of the list
     scrape_name = ti.xcom_pull(task_ids=[
         "extract_from_twitter"
-    ])
+    ])[0]
+    
     keep_columns = ["date", "timezone", "tweet", "language"]
+    
     c_df = clean_df(scrape_name, keep_columns)
-    return c_df
+    
+    clean_name = scrape_name[:-4] + "_clean" + ".csv"
+    c_df.to_csv(clean_name, index=False)
 
-def clean_data():
-    return True
+    return clean_name
+
+def _create_table():
+
+    con = psycopg2.connect(database="postgres",
+                        user="airflow",
+                        password="airflow",
+                        host="postgres",
+                        port="5432")
+
+
+    
+    ok = create_table(con, TABLE_NAME)
+    con.close()
+
+    if not ok:
+        raise ValueError("Not able to create the table")
+
+
+def add_scrape_data(ti):
+    
+    clean_name = ti.xcom_pull(task_ids=[
+        "clean_data"
+    ])[0]
+
+    c_df = pd.read_csv(clean_name)
+
+    engine = create_engine('postgresql+psycopg2://airflow:airflow@postgres:5432/postgres')
+
+    ok = add_df_to_sql(c_df, engine, TABLE_NAME)
+
+    if not ok:
+        raise ValueError("Not able to add data to the database")
+
 
 with DAG("etl_dag",
         start_date=datetime(2021, 1, 1),
@@ -41,4 +83,14 @@ with DAG("etl_dag",
             python_callable=clean_data,
         )
 
-        extract >> clean
+        create = PythonOperator(
+            task_id="create_table",
+            python_callable=_create_table,
+        )
+
+        add_data = PythonOperator(
+            task_id="add_data",
+            python_callable=add_scrape_data,
+        )
+
+        extract >> clean >> create >> add_data
